5318a2874ce7afa48d4e3d7ad547942c607698a5
Richie Loughran <richie.loughran@ericsson.com>
2019-12-16 20:57:11 +0000

    01 - Setup project and POJO Bike

---
+++ bike-registry-service/src/main/java/com/roccorichie/bikeregistryservice/BikeRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.roccorichie.bikeregistryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRegistryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeRegistryServiceApplication.class, args);
+    }
+
+}
---
+++ bike-registry-service/src/test/java/com/roccorichie/bikeregistryservice/BikeRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roccorichie.bikeregistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
