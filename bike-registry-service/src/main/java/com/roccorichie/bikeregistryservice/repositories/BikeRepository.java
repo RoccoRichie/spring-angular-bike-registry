2ac070ad4eb254534004edb6a69a5b311c29a0a2
Richie Loughran <richie.loughran@ericsson.com>
2019-12-19 15:20:20 +0000

    07 Angular UI service created (nightmare)

--- bike-registry-service/src/main/java/com/roccorichie/bikeregistryservice/repositories/BikeRepository.java
+++ bike-registry-service/src/main/java/com/roccorichie/bikeregistryservice/repositories/BikeRepository.java
@@ -4,5 +4,6 @@ import com.roccorichie.bikeregistryservice.models.Bike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // Extend JpaRepository brings all the CRUD operations
+// Replace generic type with our Bike object and its primary key
 public interface BikeRepository extends JpaRepository<Bike, Long> {
 }
